e1c35ab8bea4263fa29f2cebb2eea0768598c288
C1300089 Cesar Lucas Francelino Evangelista Junior <cesarlucasjunior93@gmail.com>
2021-07-09 14:34:38 -0300

    Adding a PlayerName component

--- main.js
+++ main.js
@@ -3,13 +3,20 @@ import "./src/styles/generics/reset.css";
 import "./src/styles/elements/base.css";
 import "./src/styles/tools/NessBorder/style.css";
 import BoardGame from "./src/objects/BoardGame";
+import PlayerName from "./src/components/PlayerName";
 
 
 
 const $root = document.querySelector('#root');
 const $htmlBoardGame = BoardGame(6);
+const $htmlPlayerName = PlayerName('PlayerOne');
 
-$root.insertAdjacentHTML('beforeend', $htmlBoardGame);
+$root.insertAdjacentHTML('beforeend', 
+    `
+    ${PlayerName('PlayerOne')}
+    ${PlayerName('PlayerTwo')}
+    ${BoardGame(6)}
+    `);
 
 $root.onclick = (param) => {
     if(param.srcElement.className === 'card-game' || param.srcElement.className === 'icon') {
